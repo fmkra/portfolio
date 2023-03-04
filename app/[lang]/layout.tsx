d7bb21a7cb7411b18adbfeba7141ed471773a1dc
kotmawszy <kotmawszy@gmail.com>
2023-03-04 17:34:20 +0100

    Add dictionaries for internatiolizations

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,14 +1,18 @@
 import './globals.css'
+import { i18n } from '../../i18n-config'
 
 export const metadata = {
     title: '',
     description: '',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export async function generateStaticParams() {
+    return i18n.locales.map((locale) => ({ lang: locale }))
+}
+
+export default function Root({ children, params }: { children: React.ReactNode; params: { lang: string } }) {
     return (
-        <html lang="en">
-            <head></head>
+        <html lang={params.lang}>
             <body>{children}</body>
         </html>
     )
